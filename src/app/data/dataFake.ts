72924ed5cbef2e3f6dd7f25316751e32b713bdcc
yydaniel1999 <yydaniel1999@gmail.com>
2023-05-30 17:25:35 -0300

    feito responsivo

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,25 +2,29 @@ export const dataFake = [
   {
     id: 1,
     photo: 'https://placehold.co/500x300',
-    title: 'bbb',
-    description: 'ccc',
+    title: 'Lorem ipsum dolor sit amet consectetur',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam voluptate odit molestiae eligendi eaque a nihil fugiat est repellat, maiores, rem magnam facilis impedit beatae, ipsum tenetur ea laboriosam exercitationem!',
   },
   {
     id: 2,
     photo: 'https://placehold.co/500x300',
-    title: 'eee',
-    description: 'fff',
+    title: 'Lorem ipsum dolor sit amet consectetur',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam voluptate odit molestiae eligendi eaque a nihil fugiat est repellat, maiores, rem magnam facilis impedit beatae, ipsum tenetur ea laboriosam exercitationem!',
   },
   {
     id: 3,
     photo: 'https://placehold.co/500x300',
-    title: 'eee',
-    description: 'fff',
+    title: 'Lorem ipsum dolor sit amet consectetur',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam voluptate odit molestiae eligendi eaque a nihil fugiat est repellat, maiores, rem magnam facilis impedit beatae, ipsum tenetur ea laboriosam exercitationem!',
   },
   {
     id: 4,
     photo: 'https://placehold.co/500x300',
-    title: 'eee',
-    description: 'fff',
+    title: 'Lorem ipsum dolor sit amet consectetur',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam voluptate odit molestiae eligendi eaque a nihil fugiat est repellat, maiores, rem magnam facilis impedit beatae, ipsum tenetur ea laboriosam exercitationem!',
   },
 ];
