cc4713cae44ba1d5945e9c7476306052d768d9af
yydaniel1999 <yydaniel1999@gmail.com>
2023-05-30 13:07:03 -0300

    componente com propriedades dinamicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css'],
 })
-export class BigCardComponent {}
+export class BigCardComponent {
+  @Input()
+  photo: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  description: string = '';
+}
